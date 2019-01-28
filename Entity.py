f7b2285f53c063ccd123144b7fe274dd1555417b
Daniel Kratzenberg <kratzenbergdan@gmail.com>
2019-01-28 10:49:33 -0500

    Implemented Event Manager Class Updated Entity to update frame by frame All game objects require an update method
    Signed-off-by: Daniel Kratzenberg <kratzenbergdan@gmail.com>

--- Entity.py
+++ Entity.py
@@ -4,49 +4,57 @@
 
 import pygame
 import random
-
+from config import *
 vec = pygame.math.Vector2
 
 
-class Entity:
+class Entity(pygame.sprite.Sprite):
     """ This is the base class from which
         Game Entities(Player, Enemy, Etc.)
         inherit from."""
 
     def __init__(self):
+        super().__init__()
+        self.image = pygame.Surface((default_size,default_size))
+        self.image.set_colorkey(pygame.color.THECOLORS['black'])
+        self.color = (random.randint(0, 255),
+                 random.randint(0, 255),
+                 random.randint(0, 255)
+                 )
+        self.rect = self.image.get_rect()
         self.position = vec(400, 400)
         self.velocity = vec(0, 0)
-        self.lateral_accel = vec(0.005, 0.0)  # accel vector pointing to the right
-        self.vertical_accel = vec(0.0, -0.005) # accel vector pointing upward
-        self.max_speed = 0.025
+        self.lateral_accel = vec(1, 0.0)  # accel vector pointing to the right
+        self.vertical_accel = vec(0.0, -1) # accel vector pointing upward
+        self.max_speed = 1
 
-    def update_pos(self, screen_size):
-        keys = pygame.key.get_pressed()
-
+    def move(self,keys, dt):
         if keys[pygame.K_w]:
-            self.velocity += self.vertical_accel
+            self.velocity += self.vertical_accel * dt
         elif keys[pygame.K_s]:
-            self.velocity -= self.vertical_accel
+            self.velocity -= self.vertical_accel  * dt
         if keys[pygame.K_d]:
-            self.velocity += self.lateral_accel
+            self.velocity += self.lateral_accel * dt
         if keys[pygame.K_a]:
-            self.velocity -= self.lateral_accel
+            self.velocity -= self.lateral_accel * dt
 
         # self.velocity.length() returns the Euclidean length of the vector
         if self.velocity.length() > self.max_speed:
             self.velocity.scale_to_length(self.max_speed)
 
         self.position += self.velocity
+        self.rect.center = (int(self.position[0]),int(self.position[1]))
 
-    def draw(self, screen_size, win):
-        color = (random.randint(0, 255),
+    def update(self, keys, dt):
+        self.move(keys,dt)
+
+        self.color = (random.randint(0, 255),
                  random.randint(0, 255),
                  random.randint(0, 255)
                  )
-        x = self.position.x
-        y = self.position.y
-        self.update_pos(screen_size)
-        pygame.draw.circle(win,
-                           color,
-                           (int(x), int(y)),
-                           10)
+
+
+
+    def draw(self, win):
+        pygame.draw.circle(self.image, self.color, (default_size // 2, default_size // 2), default_size//2)
+        win.blit(self.image,self.rect)
