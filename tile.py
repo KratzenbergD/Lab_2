cd5a45abc69c8d1cd03ec08d3a6c180b2fe83075
Daniel Kratzenberg <kratzenbergdan@gmail.com>
2019-01-30 10:13:18 -0500

    Added Debug to turn on hitbox detection
    Signed-off-by: Daniel Kratzenberg <kratzenbergdan@gmail.com>

--- tile.py
+++ tile.py
@@ -3,8 +3,20 @@ import pygame
 
 class Tile(pygame.sprite.Sprite):
 
-     def __init__(self, imageSurf, coord):
-         super().__init__()
-         self.image = imageSurf
-         self.rect = self.image.get_rect()
-         self.rect.topleft = coord
+    def __init__(self, imageSurf, coord):
+        super().__init__()
+        self.image = imageSurf
+        self.rect = self.image.get_rect()
+        self.rect.topleft = coord
+        self.debug = False
+
+    def toggleDebug(self):
+        self.debug = not self.debug
+
+    def draw(self,win):
+        if self.debug:
+            pygame.draw.rect(self.image, pygame.color.THECOLORS['red'], (0,0,self.rect.w,self.rect.h),1)
+        win.blit(self.image,self.rect)
+
+
+
