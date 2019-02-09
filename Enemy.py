b0ea328c7d6f413361fb7f75842fe287599df30a
Daniel Kratzenberg <kratzenbergdan@gmail.com>
2019-02-08 21:58:25 -0500

    -Adjusted enemy aggro rect
    Signed-off-by: Daniel Kratzenberg <kratzenbergdan@gmail.com>

--- Enemy.py
+++ Enemy.py
@@ -16,6 +16,8 @@ class Enemy(Entity):
         self.cur_hp = self.max_hp
         self.aggro_rect = pygame.rect.Rect(self.rect.left - 200, self.rect.top - 200, 400, 400)
 
+        #self.rect = pygame.rect.Rect(self.world_rect)
+
     def move(self, keys, dt):
         """ Enemy will remain stationary until player
             approaches close enough to trigger movement."""
@@ -29,5 +31,11 @@ class Enemy(Entity):
 
     def draw(self, win, cameraPos):
         super().draw(win, cameraPos)
-        pygame.draw.rect(self.image, pygame.color.THECOLORS['red'], self.aggro_rect, 1)
         win.blit(self.image, (self.rect.left - cameraPos[0], self.rect.top - cameraPos[1], self.rect.w, self.rect.h))
+        screenRect = pygame.Rect(
+            self.aggro_rect.left - cameraPos[0],
+            self.aggro_rect.top - cameraPos[1],
+            self.aggro_rect.w,
+            self.aggro_rect.h
+        )
+        pygame.draw.rect(win,pygame.color.THECOLORS['red'],screenRect,1)
